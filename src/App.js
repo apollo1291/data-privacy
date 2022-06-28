f1b14b97783b3fefb9adb1871e44281f80a22dd9
Apollo1291 <hemphilled@icloud.com>
2022-06-27 19:22:09 -0500

    Start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { useState } from 'react';
 
-function App() {
-  return (
+
+class TopNav extends Component {
+
+  renderNav(name){
+    return(
+      <li><button className="navButton" onClick={this.props.OnClick}>
+        
+        { name }
+      </button></li>
+    )
+  }
+  render(){
+    return(
+      <>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      <div class="topnav">
+  
+        <div class="nav">
+          <p id="name"> Data-Link</p>
+          <ul>
+            {this.renderNav("Home")}
+            {this.renderNav("About")}
+          </ul>
+          
+        </div>
+  
+      </div>
+      </>
+    )
+  }
+  
+}
+class MainPage extends Component{
+  
+
+  renderState(){
+    /* 
+      input: nothing
+      return: html section of webpage
+
+      decides which webpage to return based on the Page passed from App's state
+    */
+    if (this.props.Page === "Home"){
+      return(
+        <div class="urlsearch"> <p>hello</p></div>
+      ) 
+      }
+    if (this.props.Page === "About"){
+      return(
+        <div class="About"> hiiii </div>
+      )
+    } 
+  }
+
+  render(){
+    return(
+      this.renderState()
+    )
+  }
+}
+class App extends Component{
+
+  state = {Page: "Home"}
+  
+  handleClick = () => {
+    /* input: nothing
+       return: nothing
+
+       changes the state of App 
+    */ 
+    this.setState({Page: "About"})
+  }
+  
+  render(){
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <TopNav OnClick={() => this.handleClick()}/>
+    <MainPage Page = {this.state.Page}/>
     </div>
-  );
+    
+    
+  );}
+
 }
 
 export default App;
+

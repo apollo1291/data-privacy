38ab920b23efaa1329acd7effec836813fb71911
Apollo1291 <hemphilled@icloud.com>
2022-07-04 18:33:16 -0500

    Cleaned up, contains the Main render for App

--- src/App.js
+++ src/App.js
@@ -1,55 +1,9 @@
 import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
 import { useState } from 'react';
-import MainPage from './MainPage';
+import { MainPage } from './MainPage';
+import { TopNav } from './TopNav';
 
-
-function MenuOption(props){
-
-  const handleClick = () => {
-    /*
-     * Takes no arguments
-     * no return value
-     * Changes the Page state of the App webpage
-    
-    */
-    props.changePage({Page: props.name})
-
-  }
-
-  return(
-    <li><button className="navButton" onClick={handleClick}>
-        
-        { props.name }
-      </button></li>
-    )
-  
-}
-function TopNav(props){
-
-    
-  
-    return(
-      <>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-      <div class="topnav">
-  
-        <div class="nav">
-          <p id="name"> Data-Link</p>
-          <ul>
-            <MenuOption name = {"Home"} changePage={props.changePage}/>
-            <MenuOption name = {"About"} changePage={props.changePage}/>
-          </ul>
-          
-        </div>
-  
-      </div>
-      </>
-    )
-
-  
-}
 
 function App(){
 

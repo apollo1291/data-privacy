7ae16852c0df47c911edd55b0e1e312ef65cbca6
Apollo1291 <hemphilled@icloud.com>
2022-07-14 21:29:41 -0500

    +websites data; counter to track excess post req

--- api/Server.js
+++ api/Server.js
@@ -15,6 +15,37 @@ const users = [{
     password: 'apple'
 }] 
 
+const Websites = [
+    { 
+        url: "www.google.com",
+        rating: 4
+
+    },
+
+    {
+        url: 'www.mit.edu',
+        rating: 5
+    },
+    {
+        url: 'www.amazon.com',
+        rating: 1
+    },
+    {
+        url: 'www.mcdonalds.com',
+        rating: 2
+    },
+    {
+        url: 'www.yahoo.com',
+        rating: 3.45
+    },
+    
+    {
+        url: 'www.bing.com',
+        rating: -4.1
+    }
+
+]
+
 app.use(bodyParser.json())
 app.use(cors());
 
@@ -27,4 +58,11 @@ app.post('/api/user', (req, res) => {
     const user = req.body.user
     users.push(user)
     res.json("added")
+})
+let counter = 0
+app.get('/api/websites', (req, res) => {
+    console.log('app.get(api/websites...) called', counter )
+    counter++
+
+    res.json(Websites)
 })

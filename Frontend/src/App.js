2e7b481005277b7220973024a85e9d421f947ff7
Apollo1291 <hemphilled@icloud.com>
2022-07-21 22:09:09 -0500

    deleted unused imports

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { MainPage } from './components/MainPage';
 import { TopNav } from './components/TopNav';
-import { LoginPage } from './components/Pages'
+import { LoginPage } from './components/Login'
 
 
 function App(){

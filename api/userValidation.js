02870d44a42bb2107b2e2196df3ec1eb17457b97
Apollo1291 <hemphilled@icloud.com>
2022-07-26 20:45:47 -0500

    validation functions check for spaces, switched to es6 arrow notation

--- api/userValidation.js
+++ api/userValidation.js
@@ -1,14 +1,20 @@
-module.exports = {
-  isValidEmail,
-  isValidPassword,
-};
 
-function isValidEmail(email) {
+const isValidEmail = (email) => {
   return String(email)
     .toLowerCase()
     .match(/\S+@\S+\.\S+/);
 }
 
-function isValidPassword(password) {
-  return password.length > 5;
+const isValidPassword = (password) => {
+  
+  if (password.length < 5 || password.includes(' ')){
+    return false
+  } ;
+  return true
 }
+
+const isValidUsername = (username) => {
+  return !username.includes(' ') && username.length > 5 
+}
+
+module.exports = {isValidEmail, isValidPassword, isValidUsername}

4787e51968e2aeb117174fbee760d641d42007bd
Apollo1291 <hemphilled@icloud.com>
2022-07-26 20:54:51 -0500

    comments

--- site_rating/get_site_data.py
+++ site_rating/get_site_data.py
@@ -4,12 +4,14 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 
 def sites(SiteList):
-#automatically close browser after with block
+    # open chrome automated, automatically close browser after with block
     with webdriver.Chrome(r'C:\Users\hemph\Downloads\chromedriver_win32\chromedriver.exe') as browser:
         data = []
+
         for site in SiteList:
             browser.get(site)
             c = browser.get_cookies()
+            # scrape all of the cookies into a usable format
             cookie_info = [{'name': c['name'], 'secure': c['secure'], 'expires': c.get('expiry', False), 'httpOnly': c['httpOnly']} for c in c]
             
             site_info = {
--- site_rating/record_cookies.py
+++ site_rating/record_cookies.py
@@ -7,8 +7,10 @@ import psycopg2
 
     
 list_of_websites = ['https://github.com', 'https://www.youtube.com/']
+# collect all the data from websites 
 data = sites(list_of_websites)
 ratings = []
+# rate all websites data
 for d in data:
     ratings.append(rate_site(d))
 
@@ -18,21 +20,27 @@ Id = 0
 for i in range(len(ratings)):
     Id += 1
     website_data.append({'id': Id, 'url': list_of_websites[i], 'rating': ratings[i]})
+
+    # !IMPORTANT assign the id relating the cookie to the website it came from
     for cookie in data[i]['cookies']:
         cookie['url_id'] = Id
         cookie_data.append(cookie)
 
-
+# open connection to datalink_api database to insert the webscraped data
 with psycopg2.connect(database="datalink_api", user='datalink', password='data', host='localhost', port= '5432') as conn:
 
     cursor = conn.cursor()
-    
+    # id here needs to match the website relational id 
     for site in website_data:
         cursor.execute("INSERT INTO websites (id, url, rating) VALUES(%s, %s, %s)", [site['id'], site['url'], site['rating']])
 
     cursor.execute("SELECT * FROM websites")
     print(cursor.fetchall())
+
+    # cookie primary key in postgres db
     i = 1 
+
+    # changes cookie['expires'] from unix epoch time to human readable time format
     for cookie in cookie_data:
         expires = cookie['expires']
         if (expires):

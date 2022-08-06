0c35333c6b6893a4d835f628a45d25eac3ff90e3
Apollo1291 <hemphilled@icloud.com>
2022-08-06 15:18:44 -0500

    cookie report page css, rating animations, table animations

--- Frontend/src/components/pages/CookieReport.js
+++ Frontend/src/components/pages/CookieReport.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import {fetchCookies, fetchRatings} from '../../services/FrontendCookies.js'
+import {fetchCookies, fetchRatings, getColor, getWidth} from '../../services/FrontendCookies.js'
+import './cookieReport.css'
 
 export const CookieReport = (props) => {
     const [cookieData, setCookies] = useState({});
@@ -19,17 +20,53 @@ export const CookieReport = (props) => {
         return (
 
             <>
-                <div>
+                
+                    <div id='ratings-container'>
                     {cookieRatings.ratings?.map(rating => (
-                        <div key='ratings'>
-                            <p>{rating.cookie_number_rating}</p>
-                            <p>{rating.cookie_security_rating}</p>
-                            <p>{rating.cookie_expires_rating}</p>
+                        <div id ='ratings' key='ratings'>
+                            <div class='r'>
+                            <h2>Number of Cookies Rating</h2>
+                            <h2>{rating.cookie_number_rating} / 33</h2>
+                            
+                            <div class = 'bar-container'> 
+                            <div class='bar' style={{'--color': getColor(rating.cookie_number_rating), '--width': getWidth(rating.cookie_number_rating)}}></div>
+                            </div>
+                            </div>
+
+                            <div class='r'>
+                            <h2> Cookie Security Rating</h2>
+                            <h2>{rating.cookie_security_rating} / 33</h2>
+                            
+                            <div class = 'bar-container'> 
+                            <div class='bar' style={{'--color': getColor(rating.cookie_security_rating), '--width': getWidth(rating.cookie_security_rating)}}></div>
+                            </div>
+                            </div>
+
+                            <div class='r'>
+                            <h2>Expiration Date Rating</h2>
+                            <h2>{rating.cookie_expires_rating} / 33</h2>
+                            
+                            <div class = 'bar-container'> 
+                            <div class='bar' style={{'--color': getColor(rating.cookie_expires_rating), '--width': getWidth(rating.cookie_expires_rating)}}></div>
+                            </div>
+                            </div>
                         </div>
 
                     ))}
-                </div>
+                    </div>
+                
+                <div id='scan-results'>
+                <div id='table-container'>
                 <table id='cookies'>
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Secure</th>
+                        <th>HTTP Only</th>
+                        <th>Expiration Date</th>
+                    </tr>
+                    </thead>
+                    
                     <tbody>
                         {cookieData.cookies?.map(cookie => (
                             <tr key={cookie.name}>
@@ -41,6 +78,8 @@ export const CookieReport = (props) => {
                         ))}
                     </tbody>
                 </table>
+                </div>
+                </div>
             </>
         );
     };
